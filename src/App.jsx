0801a74fc88cede611448b477800476d44f38aa4
kmkan <kandpalkbusiness@gmail.com>
2026-09-13 11:28:29 -0400

    Remove YouTube from socials.

--- src/App.jsx
+++ src/App.jsx
@@ -35,12 +35,6 @@ const GitHubIcon = ({ size = 35, color = "currentColor" }) => (
     </svg>
 );
 
-const YouTubeIcon = ({ size = 35, color = "currentColor" }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill={color}>
-        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/>
-    </svg>
-);
-
 const UpArrowFAIcon = () => (
     <i className="fa fa-angle-up fa-2x" aria-hidden="true"></i>
 );
@@ -218,13 +212,11 @@ const App = () => {
     const socialLinks = [
         { icon: <LinkedInIcon />, url: 'https://www.linkedin.com/in/kandpalk' },
         { icon: <GitHubIcon />, url: 'https://github.com/kmkan' },
-        { icon: <YouTubeIcon />, url: 'https://www.youtube.com/@Kamal_Kandpal' },
     ];
 
     const footerSocialLinks = [
         { icon: <LinkedInIcon size={32} color="white" />, url: 'https://www.linkedin.com/in/kandpalk' },
         { icon: <GitHubIcon size={32} color="white" />, url: 'https://github.com/kmkan' },
-        { icon: <YouTubeIcon size={32} color="white" />, url: 'https://www.youtube.com/@Kamal_Kandpal' },
     ];
 
     const homeNavButtons = [
